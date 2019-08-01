5ccdef200d5a46a9e0623082ddff453a9b72738a
boychongzen <38745550+boychongzen18@users.noreply.github.com>
2019-08-02 01:59:07 +0700

    Add files via upload

---
+++ ip-finder.py
@@ -0,0 +1,29 @@
+import requests
+
+# Tools Priv8 Tools IP Finder
+# Tested : Windows & Termux Working
+# eMAIL  : hackingforlive@yahoo.com
+# Regard : Boychongzen aka Xroot
+
+
+print ('''\033[1m\033[93m[\033[91m!\033[93m] Tools Priv8 Proxy Status \033[1;37m
+Welcombek My Tools \n \033[1;31m
+ 
+
+ /$$$$$$ /$$$$$$$        /$$$$$$$$ /$$                 /$$                    
+|_  $$_/| $$__  $$      | $$_____/|__/                | $$                    
+  | $$  | $$  \ $$      | $$       /$$ /$$$$$$$   /$$$$$$$  /$$$$$$   /$$$$$$ 
+  | $$  | $$$$$$$/      | $$$$$   | $$| $$__  $$ /$$__  $$ /$$__  $$ /$$__  $$
+  | $$  | $$____/       | $$__/   | $$| $$  \ $$| $$  | $$| $$$$$$$$| $$  \__/
+  | $$  | $$            | $$      | $$| $$  | $$| $$  | $$| $$_____/| $$      
+ /$$$$$$| $$            | $$      | $$| $$  | $$|  $$$$$$$|  $$$$$$$| $$      
+|______/|__/            |__/      |__/|__/  |__/ \_______/ \_______/|__/      
+                                                                              
+                                                                          
+                                                       
+                                 Author - Boychongzen aka Xroot 
+                                                        
+                             
+           \033[1;34mAuthor \033[91m:\033[37m Boychongzen aka Xroot                                  
+\033[1;34mMy \033[91m:\033[37mFacebook\033[91m:\033[37m https://web.facebook.com/xroot.xroot.7 > Xroot  \n \033[1;34m    
+ ''')
